b5bf4611e571a9180633707ed6f94972a98605ac
gehelem <gilles@joag.fr>
2022-10-08 11:14:32 +0200

    add datastructure classes

---
+++ src/datastructure/elt.spec.ts
@@ -0,0 +1,7 @@
+import { Elt } from './elt';
+
+describe('Elt', () => {
+  it('should create an instance', () => {
+    expect(new Elt()).toBeTruthy();
+  });
+});
---
+++ src/datastructure/prp.spec.ts
@@ -0,0 +1,7 @@
+import { Prp } from './prp';
+
+describe('Prp', () => {
+  it('should create an instance', () => {
+    expect(new Prp()).toBeTruthy();
+  });
+});
