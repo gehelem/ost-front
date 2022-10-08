385fa5b336f6f52ee91e4f1708ad54825dbc4119
gehelem <gilles@joag.fr>
2022-10-08 16:57:27 +0200

    Add basic property list

--- src/app/module-content/module-content.component.ts
+++ src/app/module-content/module-content.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { KeyValue } from '@angular/common';
+
+import { Elt } from 'src/datastructure/elt';
+import { Prp } from 'src/datastructure/prp';
+import { Mod } from 'src/datastructure/mod';
+import { WebsocketService } from '../websocket.service';
 
 @Component({
   selector: 'app-module-content',
@@ -6,10 +12,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./module-content.component.css']
 })
 export class ModuleContentComponent implements OnInit {
-
+  @Input() mod!: string;
+  @Input() dev!: string;
+  @Input() grp!: string;
+  @Input() data: any;  
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  originalOrderMod = (a: KeyValue<string,Mod>, b: KeyValue<string,Mod>): number => {
+    return 0;
+  }
+  originalOrderPrp = (a: KeyValue<string,Prp>, b: KeyValue<string,Prp>): number => {
+    return 0;
+  }
+  originalOrderElt = (a: KeyValue<string,Elt>, b: KeyValue<string,Elt>): number => {
+    return 0;
+  }
 }
