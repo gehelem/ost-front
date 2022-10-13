167e82be1f1ad29fa7e0a682eff93e45d0385f5d
gehelem <gilles@joag.fr>
2022-10-13 14:12:40 +0200

    quicksave

--- src/app/menu-item/menu-item.component.ts
+++ src/app/menu-item/menu-item.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
 import { Mod } from '../../datastructure/mod'
 import { MenuItem } from '../../datastructure/mod'
+import { WebsocketService } from '../websocket.service';
 
 @Component({
   selector: 'app-menu-item',
@@ -10,11 +11,18 @@ import { MenuItem } from '../../datastructure/mod'
 })
 export class MenuItemComponent implements OnInit {
   @Input() items: MenuItem[] = [];
+  @Input() devcat: string='';
   @ViewChild('childMenu') public childMenu: any;
   
-  constructor(public router: Router) { }
-
+  constructor(public router: Router,public ws:WebsocketService) { }
+  
   ngOnInit(): void {
+  }
+  selectDevcatGroup(dc:string,gr:string) {
+    this.ws.datastore.currentDev=dc;
+    this.ws.datastore.currentGrp=gr;
+    console.log('************'+ dc+ "--" + gr);
+
   }
 
 }
