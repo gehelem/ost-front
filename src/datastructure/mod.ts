55d0019a65c30f56974ad1b041be8c023c61e154
gehelem <gilles@joag.fr>
2022-10-28 12:49:42 +0200

    rebuild menu if property is deleted
    could make fancier, but working

--- src/datastructure/mod.ts
+++ src/datastructure/mod.ts
@@ -114,25 +114,9 @@ export class Mod {
       Object.entries(properties).forEach(([key, value], indexp) => {
         if (this.prps[key]==undefined) {this.prps[key] = new Prp;}
         this.prps[key].setAll(value);
-        Object.entries(this.prps).forEach(([key, value], indexp) => {
-            
-            /*if (this.wmenu.has(this.prps[key].devcat)) {
-              const oldgroups=this.wmenu.get(this.prps[key].devcat);
-              if (oldgroups !== undefined) {
-                if(oldgroups.indexOf(this.prps[key].group)==-1) {
-                    oldgroups.push(this.prps[key].group);
-                    this.wmenu.set(this.prps[key].devcat,oldgroups);
-                }
-              }
-            } else {
-              const oldgroups = [this.prps[key].group];
-              this.wmenu.set(this.prps[key].devcat,oldgroups);
-            }*/
-            
-          });
-          //this.menu=this.wmenu;
 
       });
+      this.setMenu();      
     }
     setValues(modname:string,json:any) {
         var properties=json["properties"];
@@ -144,27 +128,11 @@ export class Mod {
         var properties=json["properties"];
         Object.entries(properties).forEach(([key, value], indexp) => {
           delete  this.prps[key];
-
-          Object.entries(this.prps).forEach(([key, value], indexp) => {
-            
-            /*if (this.wmenu.has(this.prps[key].devcat)) {
-              const oldgroups=this.wmenu.get(this.prps[key].devcat);
-              if (oldgroups !== undefined) {
-                if(oldgroups.indexOf(this.prps[key].group)==-1) {
-                    oldgroups.push(this.prps[key].group);
-                    this.wmenu.set(this.prps[key].devcat,oldgroups);
-                }
-              }
-            } else {
-              const oldgroups = [this.prps[key].group];
-              this.wmenu.set(this.prps[key].devcat,oldgroups);
-            }*/
-            
-            
-          });
-          //this.menu=this.wmenu;
   
         });
+        this.rootmenu=[];
+        this.setMenu();
+
     }
 
 }
