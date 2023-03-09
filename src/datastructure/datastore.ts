ea985d82b01d2abd361615154a9101eb26d88760
gehelem <gilles@joag.fr>
2023-03-10 00:49:28 +0100

    default to mainctl

--- src/datastructure/datastore.ts
+++ src/datastructure/datastore.ts
@@ -8,8 +8,8 @@ export class Datastore {
     //currentMod: string='focus1';
     //currentDev: string='Control';
     //currentGrp: string='';
-    currentMod: string='Mydummymodule';
-    currentDev: string='Examples';
+    currentMod: string='mainctl';
+    currentDev: string='Available modules';
     currentGrp: string='';
 
     setAll(json:any) {
