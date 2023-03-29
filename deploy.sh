dff4bdf6164e7be2d973aee8f2a6631a9bbbdd00
gehelem <gilles@joag.fr>
2023-03-30 01:00:04 +0200

    auto send dist

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,7 @@ sudo rm -Rf /var/www/html/*
 sudo cp -R ./dist/* /var/www/html
 sudo chown -R www-data /var/www/html/
 sudo service nginx restart
+rm /home/gilles/projets/ost-front/html.tar.gz
+cd /var/www/html
+tar -czvf /home/gilles/projets/ost-front/html.tar.gz *
+cd /home/gilles/projets/ost-front/
